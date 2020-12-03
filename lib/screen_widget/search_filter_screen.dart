aed6fcf370642231adfde31a52eb721c118d69ce
patrickchow0803 <patrickchow0803@gmail.com>
2020-12-03 16:31:28 -0500

    Added basic FilerChips widgets

--- lib/screen_widget/search_filter_screen.dart
+++ lib/screen_widget/search_filter_screen.dart
@@ -12,8 +12,45 @@ class _SearchFilterScreen extends State<SearchFilterScreen> {
       appBar: AppBar(
         title: Text('Filter your search'),
       ),
-      backgroundColor: Colors.red,
-      body: Text('Filter your search'),
+      body: Container(
+        // For scrolling functionality
+        child: ListView(
+          children: [
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Categories',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                  Wrap(
+                    // The spacing between the childen
+                    spacing: 10,
+                    children: List<Widget>.generate(10, (index) {
+                      final isSelected = index % 2 == 0;
+                      return FilterChip(
+                        label: Text('Category $index'),
+                        labelStyle: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: isSelected
+                              ? Colors.white
+                              : Theme.of(context).textTheme.bodyText1.color,
+                        ),
+                        selected: isSelected,
+                        checkmarkColor: Colors.white,
+                        onSelected: (bool selected) {},
+                      );
+                    }),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
